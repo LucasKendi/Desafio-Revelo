0f47b5cb415b63a87cf1b7aa8b6b9810ca727c2a
LucasKendi <hiranolucas@gmail.com>
2019-07-29 18:40:27 -0300

    implements post creation on controller and add post view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,21 @@
 class PostsController < ApplicationController
+    def show
+        @post = Post.find(params[:id])
+    end
+
     def new
     end
 
     def create
+        @post = Post.new(post_params)
+        if(@post.save)
+            redirect_to @post
+        else
+            render 'new'
+        end
+    end
+
+    private def post_params
+        params.require(:post).permit(:title, :text)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'home/index'
   
-  resource :posts
+  resources :posts
 
   root 'home#index'
 end
