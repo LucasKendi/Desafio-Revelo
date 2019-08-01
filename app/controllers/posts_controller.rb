34f16b23bf518da565a282f854f897dd1d1c9d9a
LucasKendi <hiranolucas@gmail.com>
2019-08-01 19:24:40 -0300

    move delete button to index and uses ajax and jQuery to change view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -37,8 +37,6 @@ class PostsController < ApplicationController
     def destroy
         @post = Post.find_by_slug(params[:slug])
         @post.destroy
-
-        redirect_to posts_path
     end
 
     private def post_params
